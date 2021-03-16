4652725b694dabc245a82413eb5b025c1afe989c
jeevesh8 <creativityinczenyoga@gmail.com>
2021-03-16 18:03:03 +0530

    ch

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from src.Tokenizers.thread_tokenizer import get_tokenizer
+from src.Tokenizers import get_tokenizer
 from src.DataLoaders.xml import load_xml_data
 from src.models.crf import crf_layer
 from src.models.single_task import get_loss_predict
